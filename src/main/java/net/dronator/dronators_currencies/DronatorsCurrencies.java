c12ef138bd559d2cfe0e1e2478fb9f53121629ea
dronator1990 <coffe.master09@gmail.com>
2023-09-21 18:40:24 +0200

    crafting

--- src/main/java/net/dronator/dronators_currencies/DronatorsCurrencies.java
+++ src/main/java/net/dronator/dronators_currencies/DronatorsCurrencies.java
@@ -9,7 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DronatorsCurrencies implements ModInitializer {
-	public static  final String MOD_ID = "dronators_currencies";
+	public static final String MOD_ID = "dronators_currencies";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
