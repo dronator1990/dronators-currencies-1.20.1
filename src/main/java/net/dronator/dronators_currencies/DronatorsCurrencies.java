f5e15f2e7d7e5f879dbebca5e546b2405ebb460f
dronator1990 <coffe.master09@gmail.com>
2023-09-23 15:22:35 +0200

    Ore Generation finally

--- src/main/java/net/dronator/dronators_currencies/DronatorsCurrencies.java
+++ src/main/java/net/dronator/dronators_currencies/DronatorsCurrencies.java
@@ -3,23 +3,30 @@ package net.dronator.dronators_currencies;
 import net.dronator.dronators_currencies.block.ModBlocks;
 import net.dronator.dronators_currencies.item.ModItemGroups;
 import net.dronator.dronators_currencies.item.ModItems;
-import net.dronator.dronators_currencies.world.gen.ModWorldGeneration;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.BiomeSelectors;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.GenerationStep;
+import net.minecraft.world.gen.feature.PlacedFeature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class DronatorsCurrencies implements ModInitializer {
 	public static final String MOD_ID = "dronators_currencies";
     public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+	public static final RegistryKey<PlacedFeature> SILVER_ORE_PLACED_KEY = RegistryKey.of(RegistryKeys.PLACED_FEATURE, new Identifier(MOD_ID,"silver_ore"));
 
 	@Override
 	public void onInitialize() {
+		BiomeModifications.addFeature(BiomeSelectors.foundInOverworld(), GenerationStep.Feature.UNDERGROUND_ORES, SILVER_ORE_PLACED_KEY);
+
 		ModItemGroups.registerItemGroups();
 
 		ModItems.registerModItems();
 		ModBlocks.registerModBlock();
-
-		ModWorldGeneration.generateModWorldGen();
 	}
 }
--- src/main/java/net/dronator/dronators_currencies/world/gen/ModWorldGeneration.java
+++
@@ -1,8 +0,0 @@
-package net.dronator.dronators_currencies.world.gen;
-
-public class ModWorldGeneration {
-    public static void generateModWorldGen() {
-        ModOreGeneration.generateOres();
-
-    }
-}
